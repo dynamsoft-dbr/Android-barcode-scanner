cad60a694f412b0c206f836ab4cff2a4fd1d3701
elemen.z <yingfashi44@qq.com>
2018-08-16 17:06:48 +0800

    add clear history list.

--- app/src/main/java/com/dynamsoft/demo/dynamsoftbarcodereaderdemo/HistoryActivity.java
+++ app/src/main/java/com/dynamsoft/demo/dynamsoftbarcodereaderdemo/HistoryActivity.java
@@ -1,11 +1,20 @@
 package com.dynamsoft.demo.dynamsoftbarcodereaderdemo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.bluelinelabs.logansquare.LoganSquare;
 import com.dynamsoft.demo.dynamsoftbarcodereaderdemo.adapter.HistoryListAdapter;
@@ -32,9 +41,21 @@ import cn.bingoogolapple.baseadapter.BGAOnItemChildClickListener;
 public class HistoryActivity extends BaseActivity implements BGAOnItemChildClickListener {
 	@BindView(R.id.rlv_history)
 	RecyclerView rlvHistory;
+	@BindView(R.id.pb_progress)
+	ProgressBar progressBar;
 	private DBRCache mCache;
 	private HistoryListAdapter historyListAdapter;
-	private ArrayList<HistoryItemBean> listItem;
+	private List<DBRImage> imageList;
+	private Handler handler =new Handler(){
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			if (imageList!=null&&imageList.size()>0){
+				historyListAdapter.clear();
+			}
+			progressBar.setVisibility(View.GONE);
+		}
+	};
 
 	@Override
 	protected int getLayoutId() {
@@ -54,8 +75,66 @@ public class HistoryActivity extends BaseActivity implements BGAOnItemChildClick
 		fillHistoryList();
 	}
 
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.findItem(R.id.menu_share).setVisible(false);
+		menu.findItem(R.id.menu_capture).setVisible(false);
+		menu.findItem(R.id.menu_file).setVisible(false);
+		menu.findItem(R.id.menu_scanning).setVisible(false);
+		menu.findItem(R.id.menu_Setting).setVisible(false);
+		menu.findItem(R.id.menu_delete).setVisible(true);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.CustomDialogTheme));
+		builder.setMessage("Clear the history list?");
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				dialogInterface.dismiss();
+			}
+		});
+		builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				progressBar.setVisibility(View.VISIBLE);
+				clearHistoryList();
+			}
+		});
+		builder.create();
+		builder.show();
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void clearHistoryList() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				List<DBRImage> imageList = LitePal.findAll(DBRImage.class);
+				if (imageList != null && imageList.size() > 0) {
+					String path = Environment.getExternalStorageDirectory() + "/dbr-preview-img";
+					File file = new File(path);
+					String[] fileNames = file.list();
+					if (fileNames != null && fileNames.length > 0) {
+						for (int i = 0; i < fileNames.length; i++) {
+							File temp=new File(path,fileNames[i]);
+							if (temp.isFile()){
+								temp.delete();
+							}
+						}
+						LitePal.deleteAll(DBRImage.class);
+					}
+
+				}
+				handler.sendEmptyMessage(0);
+			}
+		}).start();
+	}
+
 	private void fillHistoryList() {
-		List<DBRImage> imageList = LitePal.findAll(DBRImage.class);
+		imageList = LitePal.findAll(DBRImage.class);
 		Collections.reverse(imageList);
 		if (imageList.size() > 16) {
 			imageList = imageList.subList(0, 15);
